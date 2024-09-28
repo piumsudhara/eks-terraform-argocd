3af87b803fb74025417b97d22851e01db377de06
Pium Sudhara <pium.karunasena@gmail.com>
2024-09-28 20:50:12 +0530

    Test2

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ data "aws_eks_cluster_auth" "main" {
 }
 
 module "vpc" {
-  source       = "../modules/vpc"
+  source       = "./modules/vpc"
   vpc_name     = var.vpc_name
   cidr_block   = var.cidr_block
   subnet_count = var.subnet_count
